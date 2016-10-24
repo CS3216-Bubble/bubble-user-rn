f73652521117bcd0da63acee1c1e22613a9e0060
Michelle Tan <michelletanjm@gmail.com>
2016-10-24 22:06:42 +0800

    Add SettingsComponent

--- Bubble/app/components/SettingsComponent.js
+++ Bubble/app/components/SettingsComponent.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, ScrollView, View, Switch, Text } from 'react-native';
+import { List, ListItem } from 'native-base';
 
 import { Styles } from '../styles/Styles';
 
 import { Actions } from 'react-native-router-flux';
 
 export default class SettingsComponent extends Component {
-    render() {
-        return (
-            <View style={Styles.container}>
-                <Text>Settings page</Text>
+  state = {
+    isNotificationsOn: true
+  }
+
+  render() {
+      return (
+        <ScrollView>
+            <View style={styles.customListItemRight}>
+              <Text style={styles.customListItemText}>Notifications</Text>
+              <Switch
+                onValueChange={(value) => this.setState({isNotificationsOn: value})}
+                value={this.state.isNotificationsOn} />
             </View>
-        );
-    }
+        </ScrollView>
+      );
+  }
 }
+
+var styles = StyleSheet.create({
+    customListItemRight: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 10,
+      borderBottomColor: '#bbb',
+      borderBottomWidth: StyleSheet.hairlineWidth
+    },
+    customListItemText: {
+      fontSize: 16
+    }
+
+});
