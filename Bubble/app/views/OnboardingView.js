ea4a90a6ac6c2a24222bcda888b066be03199f39
Michelle Tan <michelletanjm@gmail.com>
2016-10-26 15:54:20 +0800

    Add OnboardingView

--- Bubble/app/views/OnboardingView.js
+++ Bubble/app/views/OnboardingView.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Styles } from '../styles/Styles';
+import { AppRegistry, Alert } from 'react-native';
+import AppIntro from 'react-native-app-intro';
 
 export default class OnboardingView extends Component {
-    render() {
-        return (
-            <View style={Styles.container}>
-                <Text style={Styles.welcome}>
-                    Onboarding View
-        </Text>
-                <Text style={Styles.instructions}>
-                    To get started, edit this js.
-        </Text>
-                <Text style={Styles.instructions}>
-                    For Android: {'\n'}
-                    Double tap R on your keyboard to reload,{'\n'}
-                    Shake or press menu button for dev menu
-        </Text>
-                <Text style={Styles.instructions}>
-                    For iOS: {'\n'}
-                    Press Cmd + R on your keyboard to reload,{'\n'}
-                    Shake or press Cmd + R for dev menu
-        </Text>
-            </View>
-        );
-    }
+  onSkipBtnHandle = (index) => {
+    Alert.alert('Skip');
+    console.log(index);
+  }
+  doneBtnHandle = () => {
+    Alert.alert('Done');
+  }
+  nextBtnHandle = (index) => {
+    Alert.alert('Next');
+    console.log(index);
+  }
+  onSlideChangeHandle = (index, total) => {
+    console.log(index, total);
+  }
+  render() {
+    const pageArray = [{
+      title: 'Page 1',
+      description: 'Description 1',
+      img: 'https://goo.gl/Bnc3XP',
+      imgStyle: {
+        height: 80 * 2.5,
+        width: 109 * 2.5,
+      },
+      backgroundColor: '#fa931d',
+      fontColor: '#fff',
+      level: 10,
+    }, {
+      title: 'Page 2',
+      description: 'Description 2',
+      img: 'https://goo.gl/Bnc3XP',
+      imgStyle: {
+        height: 93 * 2.5,
+        width: 103 * 2.5,
+      },
+      backgroundColor: '#a4b602',
+      fontColor: '#fff',
+      level: 10,
+    }];
+    return (
+      <AppIntro
+        onNextBtnClick={this.nextBtnHandle}
+        onDoneBtnClick={this.doneBtnHandle}
+        onSkipBtnClick={this.onSkipBtnHandle}
+        onSlideChange={this.onSlideChangeHandle}
+        pageArray={pageArray}
+      />
+    );
+  }
 }
