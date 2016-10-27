d970a64ac097c235cccf70965c4e0a56f27f0f59
Michelle Tan <michelletanjm@gmail.com>
2016-10-28 00:10:55 +0800

    Add filter for open chats, on standby for updated socket output

--- Bubble/app/components/ChatListComponent.android.js
+++ Bubble/app/components/ChatListComponent.android.js
@@ -83,13 +83,18 @@ export class ChatListComponent extends Component {
         });
 
         // Create list of chats to show
-        const chatsToShow = chatRooms.map(function (chat) {
+        const chatsToShow = chatRooms.map(function(chat) {
+
 
             const chatContainsSearchTerm =
-            (chat.roomName.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) > -1 ||
-             chat.roomDescription.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) > -1);
+              (chat.roomName.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) > -1 ||
+               chat.roomDescription.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) > -1);
 
-            if (chatContainsSearchTerm) {
+               // const filterForOpenChats =
+               //     (this.state.showOpenChatsOnly && chat.participants.indexOf(userId) > -1) ||
+               //     (!this.state.showOpenChatsOnly);
+
+               if (chatContainsSearchTerm) { // && filterForOpenChats) {
                 // Create chat card
                 return (
                     <ChatCardComponent key={chat.roomId} chat={chat} showCategoriesOnCard={this.state.showCategoriesOnCard} />
