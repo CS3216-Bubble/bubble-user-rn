aa6b85c62297d304a99f66d72bb3a568dafae584
Michelle Tan <michelletanjm@gmail.com>
2016-10-28 22:44:57 +0800

    Add editing of name

--- Bubble/app/components/ProfileComponent.js
+++ Bubble/app/components/ProfileComponent.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { StyleSheet, View, Image } from 'react-native';
-import { Text, Button } from 'native-base';
+import { Text, Button, InputGroup, Input } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Actions } from 'react-native-router-flux';
@@ -10,15 +10,48 @@ export default class ProfileComponent extends Component {
     user: PropTypes.object.isRequired,
   }
 
+  state = {
+    isEditMode: false,
+    name: this.props.user.name,
+    inputValue: this.props.user.name,
+  }
+
+  isNameValid = () => {
+    return this.state.inputValue != '';
+  }
+
+  saveName = () => {
+    console.log('save');
+    // Action to save name
+    this.setState({isEditMode: false, name: this.state.inputValue});
+  }
+
   render() {
+    console.log(this.state.isEditMode);
+    const buttonColour = this.isNameValid() ? {color:'#00C497'} : {color:'#999999'};
+
     return (
       <View style={styles.profileContainer}>
         <Image source={{ uri: this.props.user.imgSrc }} style={styles.profileContainerImage}/>
         <View style={styles.profileContainerName}>
-          <Text style={styles.profileContainerText}>{this.props.user.name}</Text>
-          <Button transparent onPress={Actions.profileFormView}>
-            <Icon size={20} name='edit' />
-          </Button>
+          { this.state.isEditMode ?
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+              <Input
+                value={this.state.inputValue}
+                placeholder='Your nickname'
+                style={{textAlign: 'center'}}
+                onChangeText={(text) => this.setState({inputValue: text})}/>
+              <Button transparent onPress={this.saveName}>
+                <Icon size={20} name='check' style={buttonColour} />
+              </Button>
+            </View>:
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+              <Text style={styles.profileContainerText}>{this.state.name}</Text>
+              <Button transparent onPress={() => this.setState({isEditMode: true})}>
+                <Icon size={20} name='edit' />
+              </Button>
+            </View>
+          }
         </View>
       </View>
     );
@@ -37,9 +70,10 @@ var styles = StyleSheet.create({
       backgroundColor: '#A7DBD8'
     },
     profileContainerName: {
+      flex: 1,
       flexDirection: 'row',
       justifyContent: 'center',
-      alignItems: 'center',
+      alignItems: 'center'
     },
     profileContainerText: {
       fontSize: 20
