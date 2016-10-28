d981736d3cbe7a013847e7afb6a4e756b7a29620
Michelle Tan <michelletanjm@gmail.com>
2016-10-28 18:51:41 +0800

    Add category headers

--- Bubble/app/globals.js
+++ Bubble/app/globals.js
@@ -1,5 +1,13 @@
 module.exports = {
     CATEGORIES: ['Rant', 'Funny', 'Nostalgia', 'Relationship', 'Advice', 'School'],
+    CATEGORY_TAGLINES: {
+      'Rant': 'Getting stuff off your chest',
+      'Funny': 'For the lulz',
+      'Nostalgia': 'Reminiscing about childhood moments',
+      'Relationship': 'Guy A or Guy B?',
+      'Advice': 'From those who have been there and done that',
+      'School': 'Our love-hate relationship'
+    },
     MIN_USERS: 2,
     MAX_USERS: 100
 };
