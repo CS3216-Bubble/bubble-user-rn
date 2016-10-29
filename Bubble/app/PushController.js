73365a8bee3237d6e30f8bf91421c960f52be926
Ng Zhi An <ngzhian@gmail.com>
2016-10-29 13:40:52 +0800

    Move push controller inside provider, we need the store

--- Bubble/app/PushController.js
+++ Bubble/app/PushController.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PushNotification from 'react-native-push-notification';
 
-export default class PushController extends Component {
+class PushController extends Component {
   componentDidMount() {
     PushNotification.configure({
       onRegister: function(token) {
+        // TODO send this token to backend
         console.log( 'TOKEN:', token );
       },
       onNotification: function(notification) {
@@ -19,3 +21,12 @@ export default class PushController extends Component {
     return {...this.props.children};
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+    socket: state.socketHandler.socket,
+  }
+}
+
+export default connect(mapStateToProps)(PushController);
