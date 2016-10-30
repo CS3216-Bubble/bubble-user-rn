febe4f3534f567e336918a3152bfa829c2ecea98
Michelle Tan <michelletanjm@gmail.com>
2016-10-31 00:32:57 +0800

    Add content for OnboardingView

--- Bubble/app/views/OnboardingView.js
+++ Bubble/app/views/OnboardingView.js
@@ -1,59 +1,154 @@
 import React, { Component } from 'react';
-import { AppRegistry, Alert } from 'react-native';
+import { StyleSheet, AppRegistry, View, Image, Text } from 'react-native';
 import { Actions, ActionConst } from 'react-native-router-flux';
+import { unsetFirstTimeUser } from '../actions/Actions';
 import { connect as connectRedux } from 'react-redux';
 import AppIntro from 'react-native-app-intro';
 
 export class OnboardingView extends Component {
+  
   onSkipBtnHandle = (index) => {
-    // this.props.onOnboardingFinished();
+    this.props.onOnboardingFinished();
     Actions.main({type: ActionConst.REPLACE});
   }
-  doneBtnHandle = () => {
-    // this.props.onOnboardingFinished();
-    Actions.main({type: ActionConst.REPLACE});
-  }
-  nextBtnHandle = (index) => {
 
+  onDoneBtnHandle = () => {
+    this.props.onOnboardingFinished();
+    Actions.main({type: ActionConst.REPLACE});
   }
-  onSlideChangeHandle = (index, total) => {
-    // console.log(index, total);
-  }
+
   render() {
-    const pageArray = [{
-      title: 'Page 1',
-      description: 'Description 1',
-      img: 'https://goo.gl/Bnc3XP',
-      imgStyle: {
-        height: 80 * 2.5,
-        width: 109 * 2.5,
+
+    const pageArray = [
+      {
+        title: 'Need to rant about life?',
+        description: '',
+        img: (<Image source={require('../img/rant.png')} />),
+        backgroundColor: '#69D2E7',
+        type: 'header'
       },
-      backgroundColor: '#fa931d',
-      fontColor: '#fff',
-      level: 10,
-    }, {
-      title: 'Page 2',
-      description: 'Description 2',
-      img: 'https://goo.gl/Bnc3XP',
-      imgStyle: {
-        height: 93 * 2.5,
-        width: 103 * 2.5,
+      {
+        title: 'Create a chatroom',
+        description: '',
+        img: (<Image style={styles.ssImg} source={require('../img/ss_create.png')} />),
+        backgroundColor: '#2980b9',
+        type: 'slide'
       },
-      backgroundColor: '#a4b602',
-      fontColor: '#fff',
-      level: 10,
-    }];
+      {
+        title: 'Talk to people who care!',
+        description: '',
+        img: (<Image style={styles.ssImg} source={require('../img/ss_chat.png')} />),
+        backgroundColor: '#2980b9',
+        type: 'slide',
+        type: 'slide'
+      },
+      {
+        title: 'Thank the people who help!',
+        description: '',
+        img: (<Image style={styles.ssImg} source={require('../img/ss_reactions.png')} />),
+        backgroundColor: '#2980b9',
+        type: 'slide'
+      },
+      {
+        title: 'Want to chat with others and hear their worries?',
+        description: '',
+        img: (<Image source={require('../img/advice.png')} />),
+        backgroundColor: '#f39c12',
+        type: 'header'
+      },
+      {
+        title: 'Browse through chats',
+        description: 'Find a topic you care about and join in!',
+        img: (<Image style={styles.ssImg} source={require('../img/ss_list.png')} />),
+        backgroundColor: '#e67e22',
+        type: 'slide'
+      },
+      {
+        title: 'Someone annoying you on chat?',
+        description: '',
+        img: (<Image source={require('../img/worried.png')} />),
+        backgroundColor: '#e74c3c',
+        type: 'header'
+      },
+      {
+        title: 'Hide them',
+        description: 'No more troll messages',
+        img: (<Image style={styles.ssImg} source={require('../img/ss_reactions3.png')} />),
+        backgroundColor: '#e74c3c',
+        type: 'slide'
+      }
+    ];
+
+    const slides = pageArray.map(function(slide, index) {
+      if (slide.type === 'header') {
+        return (
+          <View key={index} style={[styles.slide, { backgroundColor: slide.backgroundColor }]}>
+            <View level={10} style={styles.headerImgContainer}>{ slide.img }</View>
+            <View level={15}><Text style={styles.text}>{slide.title}</Text></View>
+            <View level={8}><Text style={styles.description}>{slide.description}</Text></View>
+          </View>
+        );
+      } else {
+        return (
+          <View key={index} style={[styles.slide, { backgroundColor: slide.backgroundColor }]}>
+            <View level={15}><Text style={styles.text}>{slide.title}</Text></View>
+            <View level={8}><Text style={styles.description}>{slide.description}</Text></View>
+            <View level={10} style={styles.imgContainer}>{ slide.img }</View>
+          </View>
+        );
+      }
+
+    });
+
     return (
       <AppIntro
-        onNextBtnClick={this.nextBtnHandle}
-        onDoneBtnClick={this.doneBtnHandle}
         onSkipBtnClick={this.onSkipBtnHandle}
-        onSlideChange={this.onSlideChangeHandle}
-        pageArray={pageArray}
-      />
+        onDoneBtnClick={this.onDoneBtnHandle}>
+        { slides }
+      </AppIntro>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 70,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  description: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  headerImgContainer: {
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imgContainer: {
+    height: 16 * 27,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  ssImg: {
+    width: 9 * 27,
+    height: 16 * 27
+  }
+});
 
 const mapStateToProps = (state, ownProps) => {
     return {
@@ -62,9 +157,9 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
-        // onOnboardingFinished: () => {
-        //   dispatch(unsetFirstTimeUser());
-        // }
+        onOnboardingFinished: () => {
+          dispatch(unsetFirstTimeUser());
+        }
     };
 };
 
